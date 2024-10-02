b988d6112c72c8d582063dfaf68def38a0f71e9d
zehraBal <fz.bal00@gmail.com>
2024-10-02 22:00:06 +0300

    Router added

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -3,14 +3,17 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Contact from "./pages/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Contact />
+      <Routes>
+        <Route exact path="/contact" Component={Contact} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
