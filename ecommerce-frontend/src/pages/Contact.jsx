e6acf5366b9edc62b185cf5f9f7a07b0d31022d4
zehraBal <fz.bal00@gmail.com>
2024-10-05 18:17:18 +0300

    contact page edited

--- ecommerce-frontend/src/pages/Contact.jsx
+++ ecommerce-frontend/src/pages/Contact.jsx
@@ -1,62 +1,85 @@
-export default function Contact() {
+import React from "react";
+
+const Contact = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center relative">
-      {/* Gradient Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-l from-transparent to-black opacity-75 z-10"></div>
-
-      {/* Background Image */}
-      <div className="absolute inset-0 bg-contact-bg bg-cover bg-[center_top_10%] z-0"></div>
-
-      {/* Contact Us Section */}
-      <div className="w-[497px] relative z-20 text-white">
-        <h2 className="text-4xl font-bold">CONTACT US</h2>
-        <p className="mt-4">
-          Problems trying to resolve the conflict between the two major realms
-          of Classical physics: Newtonian mechanics
-        </p>
-        <button className="mt-8 px-6 py-3 bg-[#23A6F0] hover:bg-blue-700 rounded-lg">
-          Contact Us
-        </button>
-      </div>
-
-      <div className="relative z-20 text-white flex flex-col md:flex-row w-full justify-between px-12 mt-12">
-        <div className="flex-1 grid grid-cols-2 gap-8">
-          <div>
-            <h2 className="text-lg font-semibold mb-2">Paris</h2>
-            <p>1901 Thorn ridge Cir.</p>
-            <p>75000 Paris</p>
-            <p>Phone : +451 215 215</p>
-            <p>Fax : +451 215 215</p>
-          </div>
-
-          <div>
-            <h2 className="text-lg font-semibold mb-2">New York</h2>
-            <p>2715 Ash Dr. San Jose</p>
-            <p>75000 Paris</p>
-            <p>Phone : +451 215 215</p>
-            <p>Fax : +451 215 215</p>
+    <div
+      className="relative bg-cover  min-h-svh  "
+      style={{
+        backgroundImage: 'url("/images/carousel.png")',
+        backgroundPositionY: "20%",
+        backgroundRepeat: "no-repeat",
+        //  backgroundOrigin: "border-box",
+      }}
+    >
+      <div
+        className="absolute inset-0"
+        style={{
+          background:
+            "linear-gradient(89.85deg, #000000 0.13%, #000000 0.14%, rgba(0, 0, 0, 0) 100%)",
+        }}
+      ></div>
+      <div className="relative flex justify-center items-stretch gap-8 sm:flex-col max-w-6xl mx-auto py-28 px-4 md:px-8 text-white">
+        {/* Left Side - Contact Info */}
+        <div className="min-w-[497px] flex flex-col  items-center justify-center space-y-8 text-left">
+          <div className="flex flex-col gap-9 sm:items-center sm:text-center">
+            <div>
+              <h2 className="font-bold text-left">CONTACT US</h2>
+            </div>
+            <div>
+              <p className="text-sm">
+                Problems trying to resolve the conflict between <br /> the two
+                major realms of Classical physics: <br />
+                Newtonian mechanics.
+              </p>
+            </div>
+            <div>
+              <button className="bg-[#23A6F0] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Contact Us
+              </button>
+            </div>
           </div>
         </div>
 
-        {/* Second Column */}
-        <div className="flex-1 grid grid-cols-2 gap-8">
-          <div>
-            <h2 className="text-lg font-semibold mb-2">Berlin</h2>
-            <p>4140 Parker Rd.</p>
+        {/* Right Side - Locations */}
+        <div className="min-w-[250px] flex flex-col gap-2 items-start justify-center sm:items-center ">
+          <div className="p-6 space-y-4 ">
+            <h3 className="font-bold ">Paris</h3>
+            <p className="text-xl">1901 Thorn ridge Cir.</p>
+            <div className=" w-[58px] border-b-2 border-solid border-[#23A6F0]"></div>
             <p>75000 Paris</p>
-            <p>Phone : +451 215 215</p>
-            <p>Fax : +451 215 215</p>
+            <p>Phone: +451 215 215</p>
+            <p>Fax: +451 215 215</p>
           </div>
-
-          <div>
-            <h2 className="text-lg font-semibold mb-2">London</h2>
-            <p>3517 W. Gray St. Utica</p>
+          <div className="p-6 space-y-4 sm:pr-16">
+            <h3 className="font-bold ">Berlin</h3>
+            <p className="text-xl">4140 Parker Rd.</p>
+            <div className=" w-[58px] border-b-2 border-solid border-[#23A6F0]"></div>
             <p>75000 Paris</p>
-            <p>Phone : +451 215 215</p>
-            <p>Fax : +451 215 215</p>
+            <p>Phone: +451 215 215</p>
+            <p>Fax: +451 215 215</p>
+          </div>
+        </div>
+        <div className="min-w-[250px] flex flex-col gap-2 items-start sm:items-center">
+          <div className="p-6 space-y-4">
+            <h3 className="font-bold ">New York</h3>
+            <p className="text-xl">2715 Ash Dr. San Jose,</p>
+            <div className=" w-[58px] border-b-2 border-solid border-[#23A6F0]"></div>
+            <p>75000 Paris</p>
+            <p>Phone: +451 215 215</p>
+            <p>Fax: +451 215 215</p>
+          </div>
+          <div className="p-6 space-y-4">
+            <h3 className="font-bold ">London</h3>
+            <p className="text-xl">3517 W. Gray St. Utica,</p>
+            <div className=" w-[58px] border-b-2 border-solid border-[#23A6F0]"></div>
+            <p> 75000 Paris</p>
+            <p>Phone: +451 215 215</p>
+            <p>Fax: +451 215 215</p>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Contact;
